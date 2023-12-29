2efe5baddb44f8f1fbc8f4eab6aabaef38e2b72b
Hassan <derkaouihassan19@gmail.com>
2023-12-29 19:38:41 +0100

    create many components

--- body_start_front/src/app/app.module.ts
+++ body_start_front/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Customer/home/home.component';
+import { AboutComponent } from './Customer/about/about.component';
+import { ProgramComponent } from './Customer/program/program.component';
+import { SubcriptionComponent } from './Customer/subcription/subcription.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { RegisterComponent } from './Auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProgramComponent,
+    SubcriptionComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
