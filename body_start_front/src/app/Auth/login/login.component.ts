04b65fbd94087878be1fd34612a739d479065c33
Hassan <derkaouihassan19@gmail.com>
2024-01-01 19:48:47 +0100

    login form is done

--- body_start_front/src/app/Auth/login/login.component.ts
+++ body_start_front/src/app/Auth/login/login.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthentificationService} from "../../Services/AuthManager";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginError: string = '';
+  formData!: FormGroup
+  isSubmitted: boolean = false
 
+  constructor(private authService: AuthentificationService, private router: Router, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.formData = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  login() {
+    if (this.formData.valid) {
+      this.authService.login(this.formData.value).subscribe((response: any) => {
+          this.isSubmitted = true;
+          if (response.roles) {
+            this.authService.setRoles(response.roles);
+            this.authService.setToken(response.token);
+          }
+          if (this.authService.hasRole('ROLE_ADMIN') && this.isSubmitted) {
+            this.router.navigate(['/admin/exercises']);
+          } else {
+            alert('gg')
+          }
+        },
+        (error) => {
+          console.error('Erreur d\'authentification :', error);
+          this.loginError = 'Erreur d\'authentification. Veuillez vérifier vos identifiants.';
+        });
+    }
+  }
 }
--- body_start_front/src/app/app-routing.module.ts
+++ body_start_front/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ExerciseComponent} from "./Admin/exercise_index/exercise.component";
+import {LoginComponent} from "./Auth/login/login.component";
 
 const routes: Routes = [
   {
     path:"admin/exercises", component: ExerciseComponent,
+  },
+  {
+    path:"login", component: LoginComponent,
   }
 ];
 
