74e688624db9f0a0d35a7a3c32c7fee47c53463c
Alexandre Canacaris <tiloucanac1300@gmail.com>
2024-02-06 14:31:30 +0100

    responsive de header et acceuil + quelques modif

--- js/styles.js
+++ js/styles.js
@@ -4,14 +4,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
       navToggle.addEventListener('click', toggleNav);
   }
 
-  // Gestion du redimensionnement de la fenêtre
   window.addEventListener('resize', handleResize);
-  handleResize(); // Appel initial pour fixer l'état initial correctement
+  handleResize();
 });
 
 function toggleNav() {
   var nav = document.querySelector('.nav ul');
-  if (window.innerWidth <= 1101) { // Vérifie si la largeur de la fenêtre est inférieure ou égale à 1110px
+  if (window.innerWidth <= 1101) {
       if (nav.style.display === 'block') {
           nav.style.display = 'none';
       } else {
@@ -23,9 +22,7 @@ function toggleNav() {
 function handleResize() {
   var nav = document.querySelector('.nav ul');
   if (window.innerWidth > 1101) {
-      nav.style.display = ''; // Supprime le style inline lorsque la largeur dépasse 1110px
+      nav.style.display = '';
   } else {
-      // Optionnel : Vous pouvez ici ajouter une logique pour définir un état spécifique
-      // lorsque la fenêtre est redimensionnée en dessous de 1110px, si nécessaire.
   }
 }
